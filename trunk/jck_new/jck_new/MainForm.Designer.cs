db93ae17cf713a5590879b998e3b7daabb30da25
jmq2007@gmail.com <jmq2007@gmail.com@b6b0bb59-df5f-c500-bb90-cc3690c6a4d5>
2012-11-13 14:05:20 +0000

    first

---
+++ trunk/jck_new/jck_new/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace jck_new
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dockPanel1 = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.maintool = new System.Windows.Forms.ToolStrip();
+            this.maintool_btn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.maintool.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dockPanel1
+            // 
+            this.dockPanel1.ActiveAutoHideContent = null;
+            this.dockPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockPanel1.Location = new System.Drawing.Point(0, 0);
+            this.dockPanel1.Name = "dockPanel1";
+            this.dockPanel1.RightToLeftLayout = true;
+            this.dockPanel1.Size = new System.Drawing.Size(752, 403);
+            this.dockPanel1.TabIndex = 1;
+            // 
+            // maintool
+            // 
+            this.maintool.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.maintool.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.maintool_btn,
+            this.toolStripSeparator1,
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton5,
+            this.toolStripButton3,
+            this.toolStripSeparator2,
+            this.toolStripButton4});
+            this.maintool.Location = new System.Drawing.Point(0, 0);
+            this.maintool.Name = "maintool";
+            this.maintool.Size = new System.Drawing.Size(752, 39);
+            this.maintool.TabIndex = 4;
+            this.maintool.Text = "toolStrip1";
+            // 
+            // maintool_btn
+            // 
+            this.maintool_btn.Image = ((System.Drawing.Image)(resources.GetObject("maintool_btn.Image")));
+            this.maintool_btn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.maintool_btn.Name = "maintool_btn";
+            this.maintool_btn.Size = new System.Drawing.Size(80, 36);
+            this.maintool_btn.Text = "导航条";
+            this.maintool_btn.Click += new System.EventHandler(this.maintool_btn_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(68, 36);
+            this.toolStripButton1.Text = "销售";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(68, 36);
+            this.toolStripButton2.Text = "进货";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(68, 36);
+            this.toolStripButton3.Text = "统计";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 39);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(68, 36);
+            this.toolStripButton4.Text = "帮助";
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(92, 36);
+            this.toolStripButton5.Text = "库存管理";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 381);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(752, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 403);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.maintool);
+            this.Controls.Add(this.dockPanel1);
+            this.IsMdiContainer = true;
+            this.Name = "MainForm";
+            this.Text = "M销售系统";
+            this.maintool.ResumeLayout(false);
+            this.maintool.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WeifenLuo.WinFormsUI.Docking.DockPanel dockPanel1;
+        private System.Windows.Forms.ToolStrip maintool;
+        private System.Windows.Forms.ToolStripButton maintool_btn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ trunk/jck_new/jck_new/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace jck_new
+{
+    public partial class MainForm : Form
+    {
+        #region 属性字段
+
+        private MainToolWindow mainToolWin = new MainToolWindow();
+        private SaleForm saleForm = new SaleForm();
+        private BuyForm buyForm = new BuyForm();
+
+        #endregion
+        public MainForm()
+        {
+            InitializeComponent();
+
+            mainToolWin.Show(this.dockPanel1, DockState.DockLeft);
+            saleForm.Show(this.dockPanel1);
+            buyForm.Show(this.dockPanel1);
+        }
+
+        private void maintool_btn_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
